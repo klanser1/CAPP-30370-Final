e7cbc59afe7c258084f5cf37d13b9716c11ff115
klanser1 <klanser1@nd.edu>
2013-12-16 18:12:02 -0500

    Added files

---
+++ FinalProjectJava.js
@@ -0,0 +1,14 @@
+window.onload
+console.log (window.innerWidth);
+
+ $('#change_season').on("click", function(){
+    var $central_park = $('#central_park');
+    $central_park.attr('src', 'centralparkwinter.jpg');
+    $central_park.attr('alt', 'Central Park in Winter');
+  });
+
+header = $('#header');
+	$(header).on('click', function() {
+		console.log( 'Width: ' + header.width() );
+		console.log( 'Height: ' + header.height() );
+		});
