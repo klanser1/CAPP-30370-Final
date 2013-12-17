bff91f8d533e580151ebc12bcfe8f27372ca49a3
klanser1 <klanser1@nd.edu>
2013-12-16 20:12:47 -0500

    Updated file

--- FinalProjectJava.js
+++ FinalProjectJava.js
@@ -1,14 +1,27 @@
-window.onload
-console.log (window.innerWidth);
-
  $('#change_season').on("click", function(){
     var $central_park = $('#central_park');
     $central_park.attr('src', 'centralparkwinter.jpg');
     $central_park.attr('alt', 'Central Park in Winter');
   });
 
-header = $('#header');
-	$(header).on('click', function() {
-		console.log( 'Width: ' + children.('header').width() );
-		console.log( 'Height: ' + children.('header').height() );
-		});
+ $(document).on('click','img', function() {
+  var $this = $(this);
+  console.log('Alt: ', $(this).attr("alt"));
+	console.log('width:', $(this).width());
+	console.log('height:', $(this).height());
+	});
+						
+  $(document).on('click', 'h1,h2,h3,h4', function() {
+  var $this = $(this);
+	console.log("Alt Text:", $(this).text());
+	console.log('color:', $(this).css('color'));
+	console.log('font-size:', $(this).css('color'));
+  });
+  
+
+console.log($(window).width());
+
+ $(window).resize(function(){
+    $('width').text($(window).width());
+	});
+
