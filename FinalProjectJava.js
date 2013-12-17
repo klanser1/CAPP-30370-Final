c33b8ac19c28b3e3d92498ad98eee564764d1a1e
klanser1 <klanser1@nd.edu>
2013-12-16 20:28:42 -0500

    Updated file

--- FinalProjectJava.js
+++ FinalProjectJava.js
@@ -7,15 +7,15 @@
  $(document).on('click','img', function() {
   var $this = $(this);
   console.log('Alt: ', $(this).attr("alt"));
-	console.log('width:', $(this).width());
-	console.log('height:', $(this).height());
+	console.log('Width:', $(this).width());
+	console.log('Height:', $(this).height());
 	});
 						
   $(document).on('click', 'h1,h2,h3,h4', function() {
   var $this = $(this);
 	console.log("Alt Text:", $(this).text());
-	console.log('color:', $(this).css('color'));
-	console.log('font-size:', $(this).css('color'));
+	console.log('Color:', $(this).css('color'));
+	console.log('Font-size:', $(this).css('color'));
   });
   
 
@@ -23,6 +23,6 @@ console.log($(window).width());
 
  $(window).resize(function(){
     $('width').text($(window).width());
-    console.log($(window).width());
+    console.log('Width: ' $(window).width());
 	});
 
